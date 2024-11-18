c23db1aa6a709a94632032e438d174ea79a1c3e8
amAdeel <adeeleducationpurpose@gmail.com>
2024-11-18 08:06:31 +0500

    2nd

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <section className='bg-black h-screen w-full text-white uppercase'>
       <div className='flex justify-center items-center h-full w-full flex-col gap-6'>  
         <div className=''>
-        <h1 className='text-white uppercase text-8xl font-sans font-bold tracking-tighter text-center '>Let's Talk</h1>
+        <h1 className='text-white uppercase text-8xl font-sans font-bold tracking-tighter text-center '>Lets Talk</h1>
         </div>
         <div className='flex gap-5 sm:flex-row flex-col '>
              <button className='border-2 border-white rounded-full p-2 px-4 uppercase'><Link href="thisisadeelashraf@gmail.com" target='Blank'>gmail</Link></button>
